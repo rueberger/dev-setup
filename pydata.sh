b7ccea9ab291ddac65a14ac10a9cf2bc060b0736
Andrew Berger <andberger@berkeley.edu>
2017-07-14 15:39:20 -0400

    comment all of pydata for now

--- pydata.sh
+++ pydata.sh
@@ -3,108 +3,108 @@
 # ~/pydata.sh
 
 # Removed user's cached credentials
-# This script might be run with .dots, which uses elevated privileges
-sudo -K
+# # This script might be run with .dots, which uses elevated privileges
+# sudo -K
 
-echo "------------------------------"
-echo "Setting up pip."
+# echo "------------------------------"
+# echo "Setting up pip."
 
-# Install pip
-easy_install pip
+# # Install pip
+# easy_install pip
 
-###############################################################################
-# Virtual Enviroments                                                         #
-###############################################################################
+# ###############################################################################
+# # Virtual Enviroments                                                         #
+# ###############################################################################
 
-echo "------------------------------"
-echo "Setting up virtual environments."
+# echo "------------------------------"
+# echo "Setting up virtual environments."
 
-# Install virtual environments globally
-# It fails to install virtualenv if PIP_REQUIRE_VIRTUALENV was true
-export PIP_REQUIRE_VIRTUALENV=false
-pip install virtualenv
-pip install virtualenvwrapper
+# # Install virtual environments globally
+# # It fails to install virtualenv if PIP_REQUIRE_VIRTUALENV was true
+# export PIP_REQUIRE_VIRTUALENV=false
+# pip install virtualenv
+# pip install virtualenvwrapper
 
-echo "------------------------------"
-echo "Source virtualenvwrapper from ~/.extra"
+# echo "------------------------------"
+# echo "Source virtualenvwrapper from ~/.extra"
 
-EXTRA_PATH=~/.extra
-echo $EXTRA_PATH
-echo "" >> $EXTRA_PATH
-echo "" >> $EXTRA_PATH
-echo "# Source virtualenvwrapper, added by pydata.sh" >> $EXTRA_PATH
-echo "export WORKON_HOME=~/.virtualenvs" >> $EXTRA_PATH
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> $EXTRA_PATH
-echo "" >> $BASH_PROFILE_PATH
-source $EXTRA_PATH
+# EXTRA_PATH=~/.extra
+# echo $EXTRA_PATH
+# echo "" >> $EXTRA_PATH
+# echo "" >> $EXTRA_PATH
+# echo "# Source virtualenvwrapper, added by pydata.sh" >> $EXTRA_PATH
+# echo "export WORKON_HOME=~/.virtualenvs" >> $EXTRA_PATH
+# echo "source /usr/local/bin/virtualenvwrapper.sh" >> $EXTRA_PATH
+# echo "" >> $BASH_PROFILE_PATH
+# source $EXTRA_PATH
 
-###############################################################################
-# Python 2 Virtual Enviroment                                                 #
-###############################################################################
+# ###############################################################################
+# # Python 2 Virtual Enviroment                                                 #
+# ###############################################################################
 
-echo "------------------------------"
-echo "Setting up py2-data virtual environment."
+# # echo "------------------------------"
+# # echo "Setting up py2-data virtual environment."
 
-# Create a Python2 data environment
-mkvirtualenv py2-data
-workon py2-data
+# # # Create a Python2 data environment
+# # mkvirtualenv py2-data
+# # workon py2-data
 
-# Install Python data modules
-pip install numpy
-pip install scipy
-pip install matplotlib
-pip install pandas
-pip install sympy
-pip install nose
-pip install unittest2
-pip install seaborn
-pip install scikit-learn
-pip install "ipython[all]"
-pip install bokeh
-pip install Flask
-pip install sqlalchemy
-pip install mysql-python
+# # # Install Python data modules
+# # pip install numpy
+# # pip install scipy
+# # pip install matplotlib
+# # pip install pandas
+# # pip install sympy
+# # pip install nose
+# # pip install unittest2
+# # pip install seaborn
+# # pip install scikit-learn
+# # pip install "ipython[all]"
+# # pip install bokeh
+# # pip install Flask
+# # pip install sqlalchemy
+# # pip install mysql-python
 
-###############################################################################
-# Python 3 Virtual Enviroment                                                 #
-###############################################################################
+# ###############################################################################
+# # Python 3 Virtual Enviroment                                                 #
+# ###############################################################################
 
-echo "------------------------------"
-echo "Setting up py3-data virtual environment."
+# # echo "------------------------------"
+# # echo "Setting up py3-data virtual environment."
 
-# Create a Python3 data environment
-mkvirtualenv --python=/usr/local/bin/python3 py3-data
-workon py3-data
+# # # Create a Python3 data environment
+# # mkvirtualenv --python=/usr/local/bin/python3 py3-data
+# # workon py3-data
 
-# Install Python data modules
-pip install numpy
-pip install scipy
-pip install matplotlib
-pip install pandas
-pip install sympy
-pip install nose
-pip install unittest2
-pip install seaborn
-pip install scikit-learn
-pip install "ipython[all]"
-pip install bokeh
-pip install Flask
-pip install sqlalchemy
-#pip install mysql-python  # Python 2 only, use mysqlclient instead
-pip install mysqlclient
+# # # Install Python data modules
+# # pip install numpy
+# # pip install scipy
+# # pip install matplotlib
+# # pip install pandas
+# # pip install sympy
+# # pip install nose
+# # pip install unittest2
+# # pip install seaborn
+# # pip install scikit-learn
+# # pip install "ipython[all]"
+# # pip install bokeh
+# # pip install Flask
+# # pip install sqlalchemy
+# # #pip install mysql-python  # Python 2 only, use mysqlclient instead
+# # pip install mysqlclient
 
-###############################################################################
-# Install IPython Profile
-###############################################################################
+# ###############################################################################
+# # Install IPython Profile
+# ###############################################################################
 
-echo "------------------------------"
-echo "Installing IPython Notebook Default Profile"
+# echo "------------------------------"
+# echo "Installing IPython Notebook Default Profile"
 
-# Add the IPython profile
-mkdir -p ~/.ipython
-cp -r init/profile_default/ ~/.ipython/profile_default
+# # Add the IPython profile
+# mkdir -p ~/.ipython
+# cp -r init/profile_default/ ~/.ipython/profile_default
 
-echo "------------------------------"
-echo "Script completed."
-echo "Usage: workon py2-data for Python2"
-echo "Usage: workon py3-data for Python3"
+# echo "------------------------------"
+# echo "Script completed."
+# echo "Usage: workon py2-data for Python2"
+# echo "Usage: workon py3-data for Python3"
